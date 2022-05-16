891d129b047a214a05023dfb36322435e3335f5f
Sascha Wernars <s@scha.nl>
2022-05-16 19:55:04 +0000

    Project finished

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -16,5 +16,7 @@
 @endunless
 
 </div>
-
+<div class="mt-6 p-4">
+    {{$listings->links()}}
+</div>
 @endsection
--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+<x-layout>
 
 
 @section('content')
@@ -13,34 +13,15 @@
     >
         <img
             class="w-48 mr-6 mb-6"
-            src="{{asset('images/no-image.png')}}"
+            src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}"
             alt=""
         />
 
         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
         <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-        <ul class="flex">
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Laravel</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">API</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Backend</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Vue</a>
-            </li>
-        </ul>
+
+        <x-listing-tags :tagsCsv="$listing->tags" />
+            
         <div class="text-lg my-4">
             <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
         </div>
@@ -68,8 +49,20 @@
                 >
             </div>
         </div>
+        {{-- <x-card class="mt-4 p-2 flex space-x-6">
+            <a href="/listings/{{$listing->id}}/edit">
+                <i class="fa-solid fa-pencil"></i> Edit
+            </a>
+        </x-card>
+        <x-card class="mt-4 p-2 flex space-x-6">
+            <form method="POST" action="/listings/{{$listing->id}}">
+                @csrf
+                @method('DELETE')
+                <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
+              </form>
+        </x-card> --}}
     </div>
 </x-card>
 </div>
 
-@endsection
+</x-layout>
